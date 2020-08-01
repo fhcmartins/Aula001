8a3baeb9b3577d2c945519fb6d1ea16ceaba4c9a
Flávio Martins <fhcmartins@gmail.com>
2020-08-01 15:54:53 -0300

    Criando e configurando uma segunda Activity

---
+++ app/src/main/java/com/example/aula001/KmM.java
@@ -0,0 +1,14 @@
+package com.example.aula001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KmM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_km_m);
+    }
+}
--- app/src/main/java/com/example/aula001/MainActivity.java
+++ app/src/main/java/com/example/aula001/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.aula001;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Desenvolvimento do Button btnKmMt e Associação
+        // ao Button btnKmMt da Activity (tela)
+        Button btnKmMt_prog = (Button)findViewById(R.id.btnKmMt);
+
+        Button btnMtKm_prog = (Button)findViewById(R.id.btnMtKM);
+
+        // Estrutura responsável por verificar o momento que o
+        // botão é pressionado, disparand as ações (códigos)
+        // para chamar a tela.
+
+        btnKmMt_prog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Intent informando que estamos na Activity "MainActivity"
+                // e vamos para a Activity "Intent"
+                Intent intent = new Intent(MainActivity.this, KmM.class);
+
+                // Comando que executa a intenção desenvolvida anteriormente
+                startActivity(intent);
+            }
+        });
+
+        btnMtKm_prog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MKm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
