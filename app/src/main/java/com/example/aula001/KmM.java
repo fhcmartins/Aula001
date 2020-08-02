69c2947d878b7c208c73cb59313d2b843ff1b8aa
Flávio Martins <fhcmartins@gmail.com>
2020-08-02 18:53:13 -0300

    Programação das telas KM/Metro e Metro/KM

--- app/src/main/java/com/example/aula001/KmM.java
+++ app/src/main/java/com/example/aula001/KmM.java
@@ -3,6 +3,9 @@ package com.example.aula001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class KmM extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class KmM extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_km_m);
+
+        //Declarando os componentes e fazendo associações
+        final EditText edtKm_prog = (EditText)findViewById(R.id.edtKm);
+        final EditText edtM_prog = (EditText)findViewById(R.id.edtMetro);
+        Button btnConverter_prog = (Button)findViewById(R.id.btnConverter);
+
+        //Ação do botão
+        btnConverter_prog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Declaração das variáveis
+                float km, m;
+
+                // Leitura do campo KM
+                // Conversao de uma String, pois o valor vem de um campo "Plain Text", para numeral
+                // Conversão através do "Float.parseFloat".
+                km = Float.parseFloat(edtKm_prog.getText().toString());
+
+                m = km * 1000;
+
+                // Convertendo um numeral para String
+                edtM_prog.setText(String.valueOf(m));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/aula001/MKm.java
+++ app/src/main/java/com/example/aula001/MKm.java
@@ -3,6 +3,9 @@ package com.example.aula001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MKm extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MKm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_km);
+
+        //Declarando os componentes e fazendo associações
+        final EditText edtM_prog = (EditText) findViewById(R.id.edtMetro);
+        final EditText edtKm_prog = (EditText)  findViewById(R.id.edtKm);
+        Button btnConverter_prog = (Button) findViewById(R.id.btnConverter);
+
+        //Ação do click do botão
+        btnConverter_prog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Declaração das variáveis
+                float metro, km;
+
+                //Leitura e atribuição do valor de m.
+                //Observação: O valor vem como "String" do "PlainText",
+                //e vamos converter em número através do
+                //"Float.parseFloat".
+                metro = Float.parseFloat(edtM_prog.getText().toString());
+
+                // Cálculo para converter Metro em KM
+                km = metro / 1000;
+
+                // Convertendo "km" que é um numeral para String
+                edtKm_prog.setText(String.valueOf(km));
+            }
+        });
     }
 }
